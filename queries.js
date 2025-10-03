5d8342702c94c048aec27d5dad921186627184fb
eliaslaquimane <eliasalbertolaquimane>
2025-10-03 16:21:40 +0200

    writing a querie to connect mongodb server

--- queries.js
+++ queries.js
@@ -1,2 +1,29 @@
-// writing the queries file...
+// define variable to use on connection: client, MongoClient, name of database and collection
+const { MongoClient } = require('mongodb');
+const uri = 'mongodb://localhost:27017';
+
+
+const dbName = 'plp_bookstore';
+const nameCollectio = 'books';
+
+
+// create async function with name queriesDB
+
+async function queriesDB(){
+    const client = new MongoClient(uri);
+    try {
+        // connect to the mongodb server
+
+    } catch(err){
+        console.log('error occured: ', err)
+    } finally{
+        await client.close();
+        console.log('connection closed!');
+    }
+}
+
+queriesDB()
+
+
+
 
